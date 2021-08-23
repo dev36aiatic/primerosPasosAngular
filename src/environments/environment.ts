23f665056d5e2bd20611d2d0b340f671bd64f606
Mauricio Barva <dev36@aiatic.com>
2021-08-23 18:28:35 -0500

    url para heroku cambiada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:4000',
+  baseUrl: 'https://dev36-auth.herokuapp.com',
   baseMunicipios:'https://www.datos.gov.co/resource/xdk5-pm3f.json'
 };
 
