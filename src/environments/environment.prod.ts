fc79de462fa9b3f36eb34b6afa10f96749b8f9be
Mauricio Barva <dev36@aiatic.com>
2021-08-23 17:00:26 -0500

    espacios arreglados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://dev36-auth.herokuapp.com',
+  baseUrl: 'http://localhost:4000',
   baseMunicipios:'https://www.datos.gov.co/resource/xdk5-pm3f.json'
 };
