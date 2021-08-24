26f7e7b4ff35162354bb1d799838988dcabc162a
Mauricio Barva <dev36@aiatic.com>
2021-08-24 16:41:42 -0500

    app subida a heroku y funcionando

--- src/app/usuarios/pages/video/video.component.ts
+++ src/app/usuarios/pages/video/video.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
@@ -8,26 +7,9 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class VideoComponent implements OnInit {
 
-  constructor(private _sanitizer: DomSanitizer) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  /**Funcion que recibe una URL de un video de youtube
-   * @param { string } url  - URL video de youtube
-   * @returns URL del video a incrustar
-   */
-  getVideoIframe(url: string) {
-    let video, results;
-
-    if (url === null) {
-      return '';
-    }
-    
-    results = url.match('[\\?&]v=([^&#]*)');
-    video = (results === null) ? url : results[1];
-
-    return this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + video);
   }
 
 }
