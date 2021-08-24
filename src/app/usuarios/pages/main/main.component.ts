95ee7056e6843e5f8dcccaa39c0848f887fc53f5
Mauricio Barva <dev36@aiatic.com>
2021-08-24 14:05:02 -0500

    correccion del patron del home

--- src/app/usuarios/pages/main/main.component.ts
+++ src/app/usuarios/pages/main/main.component.ts
@@ -3,23 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styles: [
-    `
-   .pattern{
-     position:fixed;
-     top:0;
-     left:0;
-     width:100%;
-     height:100%;
-     background: url('../../../../assets/pattern.jpg');
-     background-repeat: repeat-y;
-     background-size: cover;
-     background-attachment: fixed;
-     overflow-y: scroll;
-     z-index; -1 !important;
-   }
-   `
-  ]
+  styles: []
 })
 export class MainComponent implements OnInit {
 
