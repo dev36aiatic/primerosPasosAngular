06d344949d49f8f236e1f35fe3a770b52f00a053
Mauricio Barva <mauriciobarva@gmail.com>
2021-09-10 15:55:00 -0500

    modificaciones visuales a la pagina de single post

--- src/app/usuarios/usuarios-routing.module.ts
+++ src/app/usuarios/usuarios-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { HistoryComponent } from './pages/history/history.component';
@@ -8,24 +8,35 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { VideoComponent } from './pages/video/video.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { NuevoPostComponent } from './pages/nuevo-post/nuevo-post.component';
+import { SesionWpGuard } from '../guards/sesion-wp.guard';
+import { TokenWpGuard } from '../guards/token-wp.guard';
+import { IniciarSesionWpComponent } from './pages/iniciar-sesion-wp/iniciar-sesion-wp.component';
 
 
 /** Rutas hijas del modulo usuarios */
 const routes: Routes = [
   {
-    path:'',
-    component:MainComponent,
-    children:[
-      { path:'', component:HomeComponent },
-      { path:'profile', component:ProfileComponent},
-      { path:'history', component:HistoryComponent},
-      { path:'video', component:VideoComponent},
-      { path:'blog', component:BlogComponent},
-      { path:'blog/:slug', component:SinglePostComponent},
-      { path:'web-service',
-        loadChildren: ()=> import ('./pages/web-service/web-service.module').then(m => m.WebServiceModule)  
+    path: '',
+    component: MainComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'history', component: HistoryComponent },
+      { path: 'video', component: VideoComponent },
+      { path: 'blog', component: BlogComponent },
+      { path: 'blog/iniciar-sesion-wp', component: IniciarSesionWpComponent },
+      {
+        path: 'blog/añadir-post', component: NuevoPostComponent,
+        canActivate: [SesionWpGuard, TokenWpGuard],
+        canLoad: [SesionWpGuard, TokenWpGuard],
       },
-      { path:'**', redirectTo:'' }
+      { path: 'blog/:slug', component: SinglePostComponent },
+      {
+        path: 'web-service',
+        loadChildren: () => import('./pages/web-service/web-service.module').then(m => m.WebServiceModule)
+      },
+      { path: '**', redirectTo: '' }
     ]
   }
 ];
