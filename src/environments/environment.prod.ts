2d7adceadc53fe04541181a5710e9e842e4715e0
Mauricio Barva <mauriciobarva@gmail.com>
2021-09-11 09:25:23 -0500

    Rutas de peticiones a la api de wordpress modificadas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   baseUrl: 'https://dev36-auth.herokuapp.com',
   baseMunicipios:'https://www.datos.gov.co/resource/xdk5-pm3f.json',
-  wpURL:'http://dev36.latiendasigueabierta.com/wp-json/wp/v2/posts',
+  wpURL:'http://dev36.latiendasigueabierta.com/wp-json/wp/v2',
   wpToken:'http://dev36.latiendasigueabierta.com/wp-json/jwt-auth/v1/token'
 };
